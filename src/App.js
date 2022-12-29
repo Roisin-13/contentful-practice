933363a359896613ca10fd8b21a5fd0c23b95186
Roisin-13 <Roisin-13@users.noreply.github.com>
2022-12-29 16:22:15 +0000

    query added with graphql for contentful

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import { SPACE_ID, ACCESS_TOKEN } from "./key";
+
+const query = `
+{
+  examplePage(id: "1N0hGtxKYYydR05WnGjQ9R") {
+    exampleTitle
+    exampleLogo {
+      url
+    }
+  }
+}
+`;
 
 function App() {
+  const [page, setPage] = useState(null);
+  useEffect(() => {
+    window
+      .fetch(`https://graphql.contentful.com/content/v1/spaces/${SPACE_ID}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${ACCESS_TOKEN}`
+        },
+        body: JSON.stringify({ query }),
+      })
+      .then((response) => response.json())
+      .then((json) => setPage(json.data.examplePage))
+  }, []);
+
+  if (!page) {
+    return "Loading...";
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={page.exampleLogo.url} className="App-logo" alt="logo" />
+        <p>{page.exampleTitle}</p>
       </header>
     </div>
   );
 }
 
 export default App;
+
